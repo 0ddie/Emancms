8c2d91bd140299748972f1de539c3db49d2b0c95
0ddie <meo9@aber.ac.uk>
2015-04-23 13:31:25 +0100

    latest update of registration module

--- Modules/register/register_controller.php
+++ Modules/register/register_controller.php
@@ -1,72 +1,78 @@
 <?php
 
-/**
- * @return array|string
- */
 function register_controller() {
-    global $route, $mysqli;
-//            $this->mysqli = $mysqli;
+    global $route;
 
     if ($route->format == 'json') {
 
         if ($route->action == 'create') {
 
-
-
+            
+            /*
+             * Decode the Json string to find the values for Apikey, Nodeip and the timeout from the starter packet
+             */
             if (isset($_GET["apikey"])) {
                 $apikey = $_GET ["apikey"];
-//return array('content' => $apikey);
             }
             if (isset($_GET["nodeip"])) {
                 $nodeip = $_GET ["nodeip"];
-//return array('content' => $nodeip);
             }
             if (isset($_GET["timeout"])) {
                 $timeout = $_GET ["timeout"];
-//return array('content' => $timeout);
             }
-//return array ('content' => $apikey . $nodeip . $timeout);
-// print_r($apikey,$nodeip,$timeout);
-// __construct($mysqli/*,$redis,$feed*/);
-//$result = $mysqli->query("SELECT FromAddress FROM Node_reg WHERE `FromAddress` = '$nodeip'");
-//if ($result->num_rows == 1) $yes = 1; else $yes = 0;
-//return array ('content' => $yes);
-//if ($yes = "yes"){
-//$mysqli->query("INSERT INTO Node_reg (FromAddress) VALUES ('$nodeip')");
-//}
+            
+            if (strlen ($apikey)!= 32){
+                misformedError();
+                return array('content'=> " Json string wrong length");
+            }
+            if (!ip2long ($nodeip)){
+                misformedError();
+                return array('content'=> " Node ip incorrect");
+            }
+            /*
+             * Check if the node that is trying to register has been registered previously, if not add to the table Node_reg
+             */
             if (exists($nodeip) === 0) {
                 addNode($nodeip);
-                nodeMessage($nodeip);
             } else {
                 print_r("Already Registered Node");
-                nodeMessage($nodeip);
             }
-           
-        }
-    }
+                
+        }else{misformedError($route);}
+    }else{misformedError($route);}
 }
-
+/*
+ * Function to see if node is already in table Node_Reg
+ */
 function exists($nodeip) {
     global $mysqli;
     $result = $mysqli->query("SELECT FromAddress FROM Node_reg WHERE `FromAddress` = '$nodeip'");
     if ($result->num_rows === 1) {
+        nodeMessage($nodeip);
         return 1;
     } else {
         return 0;
     }
 }
-
+/*
+ * Function to add node to table Node_reg
+ */
 function addNode($nodeip) {
     global $mysqli;
     $mysqli->query("INSERT INTO Node_reg (FromAddress) VALUES ('$nodeip')");
-   // return array('content' => "Worked");
+    nodeMessage($nodeip);
+    print_r ("Node added to Node_reg");
 }
+/*
+ * Function to pull node id from table Node_reg
+ */
 function nodeMessage($nodeip) {
     global $mysqli;
-   // $nodeID = 0;
     $result = $mysqli->query("SELECT NodeID FROM `Node_reg` WHERE `FromAddress` = '$nodeip' ");
-    return array('contain' => $result);
-    
+    $row = mysqli_fetch_assoc($result);
+    print_r($row);
 }
- 
- 
+
+function misformedError(){
+    print_r("Json string misformed");
+}

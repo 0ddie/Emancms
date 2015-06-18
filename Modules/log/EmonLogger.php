d691362cda08be9f3d830148fff52fff13399054
0ddie <meo9@aber.ac.uk>
2015-06-18 13:08:38 +0200

    modified:   Modules/register/register_controller.php 	modified:   Modules/register/register_model.php 	Modules/register/register.log

--- Modules/log/EmonLogger.php
+++ Modules/log/EmonLogger.php
@@ -2,50 +2,46 @@
 /*
  All Emoncms code is released under the GNU Affero General Public License.
  See COPYRIGHT.txt and LICENSE.txt.
-
  ---------------------------------------------------------------------
  Emoncms - open source energy visualisation
  Part of the OpenEnergyMonitor project:
  http://openenergymonitor.org
  */
-
 // no direct access
 defined('EMONCMS_EXEC') or die('Restricted access');
-
-// It's important to suppress warnings here, so that adding logging is backwards compatible to old installs.
-@define("LOG4PHP_INSTALLED", include_once( 'log4php/Logger.php' ));
-if (LOG4PHP_INSTALLED)
-    @Logger::configure( $log4php_configPath );
-
 class EmonLogger
 {
-    private $concreteLogger;
-    private $loggerConfigured = false;
-    
-    public function __construct($clientFileName)
+    private $logfile = "/var/emoncms";
+    private $topic = "MAIN";
+    public function __construct()
     {
-        global $log4php_configPath;
-        if (!$log4php_configPath || !file_exists($log4php_configPath)){
-            $this->loggerConfigured = false;
-            return;
-        }
-        
-        
-        if (LOG4PHP_INSTALLED){
-            Logger::configure( $log4php_configPath );
-            $clientFileNameWithoutPath = basename($clientFileName);
-            $this->concreteLogger = Logger::getLogger($clientFileNameWithoutPath);
-            $this->loggerConfigured = true;
-        }
-    }
-
-    public function info ($message){
-        if ($this->loggerConfigured)
-            $this->concreteLogger->info($message);
     }
     
+    public function set_logfile($logfile)
+    {
+        $this->logfile = $logfile;
+    }
+    
+    public function set_topic($topic)
+    {
+        $this->topic = $topic;
+    }
+    public function info ($message){
+        if (!is_writable($this->logfile))
+            return;
+    
+        if ($fh = fopen($this->logfile,"a")) {
+            fwrite($fh,date("Y-n-j H:i:s", time())." $this->topic INFO ".$message."\n");
+            fclose($fh);
+        }
+    }
     public function warn ($message){
-        if ($this->loggerConfigured)
-            $this->concreteLogger->warn(date("Y-n-j H:i:s", time()).", ".$message);
+        if (!is_writable($this->logfile))
+            return;
+            
+        if ($fh = fopen($this->logfile,"a")) {
+            fwrite($fh,date("Y-n-j H:i:s", time())." $this->topic WARN ".$message."\n");
+            fclose($fh);
+        }
     }
 }
